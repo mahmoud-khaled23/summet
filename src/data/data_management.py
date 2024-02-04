f97d77e1c3eded114ce0556b4666fa48b1c7fc52
mahmoud-khaled01 <mahmoudkhaled1116@gmail.com>
2024-02-04 14:47:03 +0200

    update data_management

--- src/data/data_management.py
+++ src/data/data_management.py
@@ -33,40 +33,7 @@ def save_to_json(text, data_name, filename):
         add_to_json(text, data_name, filepath)
 
 
-def file_size(fpath):
-    fsize = os.stat("data/" + fpath)
-    print(f'file size = {fsize.st_size / (1024 ** 2)} MB')
-    return
-
-
-def get_openai_token():
-    """
-    get openai api token from json file in path on my device, replace it with your filepath.
-    The api token should be in my environment variables but there is a problem in my IDE.
-    :return:
-    """
-    with open("/home/ma7moud-5aled/for_vscode/tokens.json", 'rb') as file:
-        keys = json.load(file)
-
-    my_summet_key = keys["TOKENS"]["Mahmoud"]
-    # print(my_summet_key)
-    return my_summet_key
-
-
 def get_audio(audio_name):
     audio_path = "../../data/audio_files/"
     return audio_path+audio_name
-
-
-def get_json_path(json_name):
-    json_path = "../../data/extracted_text/"
-    return json_path+json_name
-
-
-def replicate_auth_token(token_name):
-    with open("/home/ma7moud-5aled/for_vscode/tokens.json", 'rb') as file:
-        keys = json.load(file)
-
-    key = keys["TOKENS"][token_name]
-    return key
 
